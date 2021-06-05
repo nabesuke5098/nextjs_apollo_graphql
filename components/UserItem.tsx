db11267d6f456fc05e6120771c7ca0849a87a7e9
nabesuke <nabesuke5098@gmail.com>
2021-06-05 13:13:26 +0900

    [modified] memo(UserItems)

--- components/UserItem.tsx
+++ components/UserItem.tsx
@@ -14,36 +14,34 @@ interface Props {
   >
 }
 
-export const UserItem: VFC<Props> = ({
-  user,
-  delete_users_by_pk,
-  setEditedUser,
-}) => {
-  console.log('UserItem renderd')
-  return (
-    <div className="my-1">
-      <span className="mr-2">{user.name}</span>
-      <p className="mr-2">{user.created_at}</p>
-      <button
-        className="mr-1 py-1 px-3 text-white bg-green-600 hover:bg-green-700 rounded-2xl focus:outline-none"
-        data-testid={`edit-${user.id}`}
-        onClick={() => setEditedUser(user)}
-      >
-        Edit
-      </button>
-      <button
-        className="py-1 px-3 text-white bg-pink-600 hover:bg-pink-700 rounded-2xl focus:outline-none"
-        data-testid={`delete-${user.id}`}
-        onClick={async () => {
-          await delete_users_by_pk({
-            variables: {
-              id: user.id,
-            },
-          })
-        }}
-      >
-        Delete
-      </button>
-    </div>
-  )
-}
+export const UserItem: VFC<Props> = memo(
+  ({ user, delete_users_by_pk, setEditedUser }) => {
+    console.log('UserItem renderd')
+    return (
+      <div className="my-1">
+        <span className="mr-2">{user.name}</span>
+        <span className="mr-2">{user.created_at}</span>
+        <button
+          className="mr-1 py-1 px-3 text-white bg-green-600 hover:bg-green-700 rounded-2xl focus:outline-none"
+          data-testid={`edit-${user.id}`}
+          onClick={() => setEditedUser(user)}
+        >
+          Edit
+        </button>
+        <button
+          className="py-1 px-3 text-white bg-pink-600 hover:bg-pink-700 rounded-2xl focus:outline-none"
+          data-testid={`delete-${user.id}`}
+          onClick={async () => {
+            await delete_users_by_pk({
+              variables: {
+                id: user.id,
+              },
+            })
+          }}
+        >
+          Delete
+        </button>
+      </div>
+    )
+  }
+)
