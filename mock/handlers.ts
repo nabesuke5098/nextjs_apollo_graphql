f15aba4fc294385879f2eebd18dbe57cbb5bde93
nabesuke <nabesuke5098@gmail.com>
2021-07-24 19:33:31 +0900

    [modified]mock/handlers.ts

--- mock/handlers.ts
+++ mock/handlers.ts
@@ -1,1 +1,17 @@
 import { graphql } from 'msw'
+export const handlers = [
+  graphql.query('GetUsers', (req, res, ctx) => {
+    return res(
+      ctx.data({
+        users: [
+          {
+            __typename: 'users',
+            id: 'b6137849-7f1d-c2db-e609-22056fb86db3',
+            name: 'Test user A',
+            created_at: '2021-01-13T18:06:46.412969+00:00',
+          }
+        ]
+      })
+    )
+  })
+]
