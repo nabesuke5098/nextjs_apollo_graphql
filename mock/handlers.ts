670fe309fc7c8a48c0b570a65a6ba6d10103700b
nabesuke <nabesuke5098@gmail.com>
2021-07-26 08:06:46 +0900

    [add]dummy

--- mock/handlers.ts
+++ mock/handlers.ts
@@ -9,8 +9,14 @@ export const handlers = [
             id: 'b6137849-7f1d-c2db-e609-22056fb86db3',
             name: 'Test user A',
             created_at: '2021-01-13T18:06:46.412969+00:00',
-          }
-        ]
+          },
+          {
+            __typename: 'users',
+            id: '2b07950f-9959-1bc7-834d-5656e4aeaac2',
+            name: 'Test user B',
+            created_at: '2021-02-13T18:06:46.412969+00:00',
+          },
+        ],
       })
     )
   }),
