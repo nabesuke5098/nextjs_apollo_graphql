3f7a3aa446dbe9f1e9713cae227a83de37ec25b2
nabesuke <nabesuke5098@gmail.com>
2021-07-26 08:41:16 +0900

    [add]NavBar.test.tsx

---
+++ __tests__/NavBar.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import userEvent from '@testing-library/user-event'
+import { getPage, initTestHelpers } from 'next-page-tester'
+import { setupServer } from 'msw/node'
+import { handlers } from '../mock/handlers'
+import 'setimmediate'
+
+initTestHelpers()
+
+const server = setupServer(...handlers)
+
+beforeAll(() => {
+  server.listen()
+})
+afterEach(() => {
+  server.resetHandlers()
+  cleanup()
+})
+afterAll(() => {
+  server.close()
+})
+
+describe('Navigation Test Cases', () => {
+  it('Should route to selected page in navbar', async () => {
+    const { page } = await getPage({
+      route: '/',
+    })
+    render(page)
+    expect(await screen.findByText('Next.js + GraphQL')).toBeInTheDocument()
+  })
+})
